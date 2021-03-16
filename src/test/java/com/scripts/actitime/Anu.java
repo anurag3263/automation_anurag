bf8147af26fd7dc9085f9848dd8ad52660c8241a
Anurag <Anurag@ANURAG24>
2021-03-16 11:21:51 +0530

    new class

---
+++ src/test/java/com/scripts/actitime/Anu.java
@@ -0,0 +1,12 @@
+package com.scripts.actitime;
+
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+public class Anu {
+@Test
+public void forgot()
+{
+	Reporter.log("forgot my name", true);
+}
+}
